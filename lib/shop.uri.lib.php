b8d9ccd60812b27f7fef6e00c57d47642ddc2dc1
thisgun <thisgun@naver.com>
2018-10-24 18:35:37 +0900

    uri.lib 파일 이름 수정

---
+++ lib/shop.uri.lib.php
@@ -0,0 +1,113 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+
+function get_pretty_shop_url($url, $folder, $no='', $query_string='', $action=''){
+    global $g5, $config;
+
+    if( $folder !== 'shop' ){
+        return $url;
+    }
+
+    $segments = array();
+    $url = $add_query = '';
+    
+    if( $config['cf_bbs_rewrite'] ){
+        $segments[0] = G5_URL;
+        $segments[1] = urlencode($folder);
+
+        if( $config['cf_bbs_rewrite'] > 1 ){
+            $item = get_shop_item($no, true);
+            $segments[2] = $item['it_seo_title'] ? urlencode($item['it_seo_title']).'/' : urlencode($no);
+        } else {
+            $segments[2] = urlencode($no);
+        }
+
+        if($query_string) {
+            // If the first character of the query string is '&', replace it with '?'.
+            if(substr($query_string, 0, 1) == '&') {
+                $add_query = preg_replace("/\&amp;/", "?", $query_string, 1);
+            } else {
+                $add_query = '?'. $query_string;
+            }
+        }
+    } else {
+        $url = G5_SHOP_URL. '/item.php';
+        if($no) {
+            $url .= '?'. urlencode($no);
+        }
+        if($query_string) {
+            $url .= ($no ? '?' : '&amp;'). $query_string;
+        }
+
+        $segments[0] = $url;
+    }
+
+    return implode('/', $segments).$add_query;
+}
+
+function add_shop_nginx_conf_rules($get_path_url, $base_path, $return_string=false){
+
+    $add_rules = array();
+
+    return implode("\n", $add_rules);
+
+}
+
+function add_shop_mod_rewrite_rules($get_path_url, $base_path, $return_string=false){
+
+    $add_rules = array();
+    
+    $add_rules[] = 'RewriteRule ^shop/([0-9a-zA-Z_]+)$  '.G5_SHOP_DIR.'/item.php?it_id=$1&rewrite=1  [L]';
+    $add_rules[] = 'RewriteRule ^shop/([^/]+)/$  '.G5_SHOP_DIR.'/item.php?it_seo_title=$1&rewrite=1  [L]';
+
+    return implode("\n", $add_rules);
+
+}
+
+function add_shop_admin_dbupgrade($is_check){
+    global $g5;
+
+    // 내용 관리 짧은 주소
+    $sql = " SHOW COLUMNS FROM `{$g5['g5_shop_item_table']}` LIKE 'it_seo_title' ";
+    $row = sql_fetch($sql);
+
+    if( !$row ){
+        sql_query("ALTER TABLE `{$g5['g5_shop_item_table']}`
+                    ADD `it_seo_title` varchar(200) NOT NULL DEFAULT '' AFTER `it_name`,
+                    ADD INDEX `it_seo_title` (`it_seo_title`);
+        ", false);
+
+        $is_check = true;
+    }
+
+    return $is_check;
+
+}
+
+function shop_exist_check_seo_title($seo_title, $type, $shop_item_table, $it_id){
+    
+    $sql = "select it_seo_title FROM {$shop_item_table} WHERE it_seo_title = '".sql_real_escape_string($seo_title)."' AND it_id <> '$it_id' limit 1";
+    $row = sql_fetch($sql, false);
+
+    if( $row['it_seo_title'] ){
+        return 'is_exists';
+    }
+
+    return '';
+}
+
+function shop_seo_title_update($it_id){
+    global $g5;
+
+    $item = get_shop_item($it_id, true);
+
+    if( ! $item['it_seo_title'] && $item['it_name'] ){
+        $it_seo_title = exist_seo_title_recursive('shop', generate_seo_title($item['it_name']), $g5['g5_shop_item_table'], $item['it_id']);
+
+        if( $it_seo_title ){
+            $sql = " update `{$g5['g5_shop_item_table']}` set it_seo_title = '{$it_seo_title}' where it_id = '{$item['it_id']}' ";
+            sql_query($sql);
+        }
+    }
+}
+?>
