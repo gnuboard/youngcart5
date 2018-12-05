6e982ee04f5a44042a6f883d049c5eaf2665b832
leebokyung <ipuiz202@naver.com>
2018-12-05 09:47:05 +0900

    54 영카트 시작

---
+++ theme/54/mobile/shop/shop.head.php
@@ -0,0 +1,74 @@
+<?php
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+
+include_once(G5_THEME_PATH.'/head.sub.php');
+include_once(G5_LIB_PATH.'/outlogin.lib.php');
+include_once(G5_LIB_PATH.'/visit.lib.php');
+include_once(G5_LIB_PATH.'/connect.lib.php');
+include_once(G5_LIB_PATH.'/popular.lib.php');
+include_once(G5_LIB_PATH.'/latest.lib.php');
+?>
+
+<header id="hd">
+    <?php if ((!$bo_table || $w == 's' ) && defined('_INDEX_')) { ?><h1><?php echo $config['cf_title'] ?></h1><?php } ?>
+
+    <div id="skip_to_container"><a href="#container">본문 바로가기</a></div>
+
+    <?php if(defined('_INDEX_')) { // index에서만 실행
+        include G5_MOBILE_PATH.'/newwin.inc.php'; // 팝업레이어
+    } ?>
+    <ul id="hd_mb">
+        <li><a href="<?php echo G5_URL; ?>/">커뮤니티</a></li>
+        <?php if ($is_member) { ?>
+        <?php if ($is_admin) {  ?>
+        <li><a href="<?php echo G5_ADMIN_URL ?>/shop_admin/"><b>관리자</b></a></li>
+        <?php } else { ?>
+        <li><a href="<?php echo G5_BBS_URL; ?>/member_confirm.php?url=register_form.php">정보수정</a></li>
+        <?php } ?>
+        <li><a href="<?php echo G5_BBS_URL; ?>/logout.php?url=shop">로그아웃</a></li>
+        <?php } else { ?>
+        <li><a href="<?php echo G5_BBS_URL; ?>/login.php?url=<?php echo $urlencode; ?>">로그인</a></li>
+        <li><a href="<?php echo G5_BBS_URL ?>/register.php" id="snb_join">회원가입</a></li>
+        <?php } ?>
+        <li><a href="<?php echo G5_SHOP_URL; ?>/mypage.php">마이페이지</a></li>
+    </ul>
+
+    <div id="hd_wr">
+        <div id="logo"><a href="<?php echo G5_SHOP_URL; ?>/"><img src="<?php echo G5_DATA_URL; ?>/common/mobile_logo_img" alt="<?php echo $config['cf_title']; ?> 메인"></a></div>
+        <div id="hd_btn">
+            <button type="button" id="btn_hdcate"><i class="fa fa-bars" aria-hidden="true"></i><span class="sound_only">분류</span></button>
+            <a href="<?php echo G5_SHOP_URL; ?>/cart.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span class="sound_only">장바구니</span><span class="cart-count"><?php echo get_boxcart_datas_count(); ?></span></a>
+
+        </div>
+    </div>
+    <?php include_once(G5_THEME_MSHOP_PATH.'/category.php'); // 분류 ?>
+
+
+    <script>
+    $( document ).ready( function() {
+        var jbOffset = $( '#hd_wr' ).offset();
+        $( window ).scroll( function() {
+            if ( $( document ).scrollTop() > jbOffset.top ) {
+                $( '#hd_wr' ).addClass( 'fixed' );
+            }
+            else {
+                $( '#hd_wr' ).removeClass( 'fixed' );
+            }
+        });
+    });
+
+    $("#btn_hdcate").on("click", function() {
+        $("#category").show();
+    });
+
+    $(".menu_close").on("click", function() {
+        $(".menu").hide();
+    });
+     $(".cate_bg").on("click", function() {
+        $(".menu").hide();
+    });
+   </script>
+</header>
+
+<div id="container">
+    <?php if ((!$bo_table || $w == 's' ) && !defined('_INDEX_')) { ?><h1 id="container_title"><?php echo $g5['title'] ?></h1><?php } ?>
---
+++ theme/54/shop/shop.tail.php
@@ -0,0 +1,81 @@
+<?php
+if (!defined("_GNUBOARD_")) exit; // 개별 페이지 접근 불가
+
+if (G5_IS_MOBILE) {
+    include_once(G5_THEME_MSHOP_PATH.'/shop.tail.php');
+    return;
+}
+
+$admin = get_admin("super");
+
+// 사용자 화면 우측과 하단을 담당하는 페이지입니다.
+// 우측, 하단 화면을 꾸미려면 이 파일을 수정합니다.
+?>
+
+    </div>
+    <!-- } 콘텐츠 끝 -->
+
+<!-- 하단 시작 { -->
+</div>
+
+<div id="ft">
+    <div class="ft_wr">
+        <ul class="ft_ul">
+            <li><a href="<?php echo get_pretty_url('content', 'company'); ?>">회사소개</a></li>
+            <li><a href="<?php echo get_pretty_url('content', 'provision'); ?>">서비스이용약관</a></li>
+            <li><a href="<?php echo get_pretty_url('content', 'privacy'); ?>">개인정보처리방침</a></li>
+            <li><a href="<?php echo get_device_change_url(); ?>">모바일버전</a></li>
+        </ul>
+        
+        <a href="<?php echo G5_SHOP_URL; ?>/" id="ft_logo"><img src="<?php echo G5_DATA_URL; ?>/common/logo_img2" alt="처음으로"></a>
+
+        <div class="ft_info">
+            <span><b>회사명</b> <?php echo $default['de_admin_company_name']; ?></span>
+            <span><b>주소</b> <?php echo $default['de_admin_company_addr']; ?></span><br>
+            <span><b>사업자 등록번호</b> <?php echo $default['de_admin_company_saupja_no']; ?></span>
+            <span><b>대표</b> <?php echo $default['de_admin_company_owner']; ?></span>
+            <span><b>전화</b> <?php echo $default['de_admin_company_tel']; ?></span>
+            <span><b>팩스</b> <?php echo $default['de_admin_company_fax']; ?></span><br>
+            <!-- <span><b>운영자</b> <?php echo $admin['mb_name']; ?></span><br> -->
+            <span><b>통신판매업신고번호</b> <?php echo $default['de_admin_tongsin_no']; ?></span>
+            <span><b>개인정보 보호책임자</b> <?php echo $default['de_admin_info_name']; ?></span>
+
+            <?php if ($default['de_admin_buga_no']) echo '<span><b>부가통신사업신고번호</b> '.$default['de_admin_buga_no'].'</span>'; ?><br>
+            Copyright &copy; 2001-2013 <?php echo $default['de_admin_company_name']; ?>. All Rights Reserved.
+        </div>
+
+        <div class="ft_cs">
+            <h2>고객센터</h2>
+            <strong>02-123-1234</strong>
+            <p>월-금 am 9:00 - pm 05:00<br>점심시간 : am 12:00 - pm 01:00</p>
+        </div>
+        <button type="button" id="top_btn"><i class="fa fa-arrow-up" aria-hidden="true"></i><span class="sound_only">상단으로</span></button>
+        <script>
+        
+        $(function() {
+            $("#top_btn").on("click", function() {
+                $("html, body").animate({scrollTop:0}, '500');
+                return false;
+            });
+        });
+        </script>
+    </div>
+
+
+</div>
+
+<?php
+$sec = get_microtime() - $begin_time;
+$file = $_SERVER['SCRIPT_NAME'];
+
+if ($config['cf_analytics']) {
+    echo $config['cf_analytics'];
+}
+?>
+
+<script src="<?php echo G5_JS_URL; ?>/sns.js"></script>
+<!-- } 하단 끝 -->
+
+<?php
+include_once(G5_THEME_PATH.'/tail.sub.php');
+?>
