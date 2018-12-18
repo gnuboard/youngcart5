3f3255a7cf8163499f0331dd2889f97c3ca84d42
leebokyung <ipuiz202@naver.com>
2018-12-18 12:02:54 +0900

    마이페이지, 주문내역 수정중

--- shop/mypage.php
+++ shop/mypage.php
@@ -43,22 +43,41 @@ for($k=0; $cp=sql_fetch_array($res); $k++) {
     <!-- 회원정보 개요 시작 { -->
     <section id="smb_my_ov">
         <h2>회원정보 개요</h2>
-        <strong class="my_ov_name"><img src="<?php echo G5_THEME_IMG_URL ;?>/no_profile.gif" alt="프로필이미지"> <?php echo $member['mb_name']; ?></strong>
-        <dl class="cou_pt">
-            <dt>보유포인트</dt>
-            <dd><a href="<?php echo G5_BBS_URL; ?>/point.php" target="_blank" class="win_point"><?php echo number_format($member['mb_point']); ?></a> 점</dd>
-            <dt>보유쿠폰</dt>
-            <dd><a href="<?php echo G5_SHOP_URL; ?>/coupon.php" target="_blank" class="win_coupon"><?php echo number_format($cp_count); ?></a></dd>
-        </dl>
-        <div id="smb_my_act">
-            <ul>
-                <?php if ($is_admin == 'super') { ?><li><a href="<?php echo G5_ADMIN_URL; ?>/" class="btn_admin">관리자</a></li><?php } ?>
-                <li><a href="<?php echo G5_BBS_URL; ?>/memo.php" target="_blank" class="win_memo btn01">쪽지함</a></li>
-                <li><a href="<?php echo G5_BBS_URL; ?>/member_confirm.php?url=register_form.php" class="btn01">회원정보수정</a></li>
-                <li><a href="<?php echo G5_BBS_URL; ?>/member_confirm.php?url=member_leave.php" onclick="return member_leave();" class="btn01">회원탈퇴</a></li>
-            </ul>
+        
+        <div class="smb_me">
+	        <strong class="my_ov_name"><img src="<?php echo G5_THEME_IMG_URL ;?>/no_profile.gif" alt="프로필이미지"><br><?php echo $member['mb_name']; ?></strong><br>
+	        <a href="<?php echo G5_BBS_URL ?>/member_confirm.php?url=register_form.php" id="s_ol_after_info">정보수정</a>
+	        <a href="<?php echo G5_BBS_URL ?>/logout.php" id="ol_after_logout">로그아웃</a>
         </div>
-
+        
+        <ul id="smb_private">
+	    	<li>
+	            <a href="<?php echo G5_BBS_URL ?>/point.php" target="_blank" class="win_point">
+					<i class="fa fa-database" aria-hidden="true"></i>포인트
+					<strong><?php echo number_format($member['mb_point']); ?></strong>
+	            </a>
+	        </li>
+	        <li>
+	        	<a href="<?php echo G5_SHOP_URL ?>/coupon.php" target="_blank" class="win_coupon">
+	        		<i class="fa fa-ticket" aria-hidden="true"></i>쿠폰
+	        		<strong><?php echo number_format($cp_count); ?></strong>
+	        	</a>
+	        </li>
+	        <li>
+	            <a href="<?php echo G5_BBS_URL ?>/memo.php" target="_blank" class="win_memo">
+	            	<i class="fa fa-envelope-o" aria-hidden="true"></i><span class="sound_only">안 읽은 </span>쪽지
+	                <strong><?php echo $memo_not_read ?></strong>
+	            </a>
+	        </li>
+	        <li>
+	            <a href="<?php echo G5_BBS_URL ?>/scrap.php" target="_blank" class="win_scrap">
+	            	<i class="fa fa-thumb-tack" aria-hidden="true"></i>스크랩
+	            	<strong class="scrap">0</strong>
+	            </a>
+	        </li>
+	    </ul>
+	    
+        <h3>내정보</h3>
         <dl class="op_area">
             <dt>연락처</dt>
             <dd><?php echo ($member['mb_tel'] ? $member['mb_tel'] : '미등록'); ?></dd>
@@ -71,78 +90,71 @@ for($k=0; $cp=sql_fetch_array($res); $k++) {
             <dt id="smb_my_ovaddt">주소</dt>
             <dd id="smb_my_ovaddd"><?php echo sprintf("(%s%s)", $member['mb_zip1'], $member['mb_zip2']).' '.print_address($member['mb_addr1'], $member['mb_addr2'], $member['mb_addr3'], $member['mb_addr_jibeon']); ?></dd>
         </dl>
-        <div class="my_ov_btn"><button type="button" class="btn_op_area"><i class="fa fa-caret-up" aria-hidden="true"></i><span class="sound_only">상세정보 보기</span></button></div>
-
+        
+        <a href="<?php echo G5_BBS_URL; ?>/member_confirm.php?url=member_leave.php" onclick="return member_leave();" class="withdrawal">회원탈퇴</a>
     </section>
-    <script>
-    
-        $(".btn_op_area").on("click", function() {
-            $(".op_area").toggle();
-            $(".fa-caret-up").toggleClass("fa-caret-down")
-        });
-
-    </script>
     <!-- } 회원정보 개요 끝 -->
 
-    <!-- 최근 주문내역 시작 { -->
-    <section id="smb_my_od">
-        <h2>최근 주문내역</h2>
-        <?php
-        // 최근 주문내역
-        define("_ORDERINQUIRY_", true);
-
-        $limit = " limit 0, 5 ";
-        include G5_SHOP_PATH.'/orderinquiry.sub.php';
-        ?>
-
-        <div class="smb_my_more">
-            <a href="./orderinquiry.php">더보기</a>
-        </div>
-    </section>
-    <!-- } 최근 주문내역 끝 -->
-
-    <!-- 최근 위시리스트 시작 { -->
-    <section id="smb_my_wish">
-        <h2>최근 위시리스트</h2>
-
-        <div class="list_02">
-            <ul>
-
-            <?php
-            $sql = " select *
-                       from {$g5['g5_shop_wish_table']} a,
-                            {$g5['g5_shop_item_table']} b
-                      where a.mb_id = '{$member['mb_id']}'
-                        and a.it_id  = b.it_id
-                      order by a.wi_id desc
-                      limit 0, 8 ";
-            $result = sql_query($sql);
-            for ($i=0; $row = sql_fetch_array($result); $i++)
-            {
-                $image = get_it_image($row['it_id'], 230, 230, true);
-            ?>
-
-            <li>
-                <div class="smb_my_img"><?php echo $image; ?></div>
-                <div class="smb_my_tit"><a href="./item.php?it_id=<?php echo $row['it_id']; ?>"><?php echo stripslashes($row['it_name']); ?></a></div>
-                <div class="smb_my_date"><?php echo $row['wi_time']; ?></div>
-            </li>
-
-            <?php
-            }
-
-            if ($i == 0)
-                echo '<li class="empty_li">보관 내역이 없습니다.</li>';
-            ?>
-            </ul>
-        </div>
-
-        <div class="smb_my_more">
-            <a href="./wishlist.php">더보기</a>
-        </div>
-    </section>
-    <!-- } 최근 위시리스트 끝 -->
-
+	<div id="smb_my_list">
+	    <!-- 최근 주문내역 시작 { -->
+	    <section id="smb_my_od">
+	        <h2>최근 주문내역</h2>
+	        <?php
+	        // 최근 주문내역
+	        define("_ORDERINQUIRY_", true);
+	
+	        $limit = " limit 0, 5 ";
+	        include G5_SHOP_PATH.'/orderinquiry.sub.php';
+	        ?>
+	
+	        <div class="smb_my_more">
+	            <a href="./orderinquiry.php">더보기</a>
+	        </div>
+	    </section>
+	    <!-- } 최근 주문내역 끝 -->
+	
+	    <!-- 최근 위시리스트 시작 { -->
+	    <section id="smb_my_wish">
+	        <h2>최근 위시리스트</h2>
+	
+	        <div class="list_02">
+	            <ul>
+	
+	            <?php
+	            $sql = " select *
+	                       from {$g5['g5_shop_wish_table']} a,
+	                            {$g5['g5_shop_item_table']} b
+	                      where a.mb_id = '{$member['mb_id']}'
+	                        and a.it_id  = b.it_id
+	                      order by a.wi_id desc
+	                      limit 0, 8 ";
+	            $result = sql_query($sql);
+	            for ($i=0; $row = sql_fetch_array($result); $i++)
+	            {
+	                $image = get_it_image($row['it_id'], 230, 230, true);
+	            ?>
+	
+	            <li>
+	                <div class="smb_my_img"><?php echo $image; ?></div>
+	                <div class="smb_my_tit"><a href="./item.php?it_id=<?php echo $row['it_id']; ?>"><?php echo stripslashes($row['it_name']); ?></a></div>
+	                <div class="smb_my_date"><?php echo $row['wi_time']; ?></div>
+	            </li>
+	
+	            <?php
+	            }
+	
+	            if ($i == 0)
+	                echo '<li class="empty_li">보관 내역이 없습니다.</li>';
+	            ?>
+	            </ul>
+	        </div>
+	
+	        <div class="smb_my_more">
+	            <a href="./wishlist.php">더보기</a>
+	        </div>
+	    </section>
+	    <!-- } 최근 위시리스트 끝 -->
+	</div>
 </div>
 
 <script>
