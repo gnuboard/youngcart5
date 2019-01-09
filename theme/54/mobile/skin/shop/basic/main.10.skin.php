eb7d5a054f5edba7424062e7d85ace7efe7fa9f6
sora90224 <sora92024@naver.com>
2019-01-09 14:22:52 +0900

    모바일 리스트 수정

--- theme/54/mobile/skin/shop/basic/main.10.skin.php
+++ theme/54/mobile/skin/shop/basic/main.10.skin.php
@@ -27,13 +27,12 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
             echo "<ul class=\"sct sct_10\">\n";
         }
     }
-
-    if($i % $this->list_mod == 0)
+   if($i % $this->list_mod == 0)
         $li_clear = ' sct_clear';
     else
         $li_clear = '';
 
-    echo "<li class=\"sct_li{$li_clear}\"$li_width_style><div class=\"li_wr\">\n";
+    echo "<li class=\"sct_li{$li_clear}\"$li_width_style><div class=\"li_wr is_view_type_list\">\n";
 
     if ($this->href) {
         echo "<div class=\"sct_img\"><a href=\"{$this->href}{$row['it_id']}\">\n";
@@ -47,12 +46,11 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
         echo "</a></div>\n";
     }
 
- 	//별점
-	if ($this->href) {
+   //별점
+    if ($this->href) {
         echo "<div class=\"sct_star\"><img src=\"\" alt=\"별점 4점\"></div>\n";
     }
 
-    
     if ($this->view_it_id) {
         echo "<div class=\"sct_id\">&lt;".stripslashes($row['it_id'])."&gt;</div>\n";
     }
@@ -68,44 +66,44 @@ for ($i=0; $row=sql_fetch_array($result); $i++) {
     if ($this->href) {
         echo "</a></div>\n";
     }
- 
-    if ($this->view_it_basic && $row['it_basic']) {
+
+	if ($this->view_it_basic && $row['it_basic']) {
         echo "<div class=\"sct_basic\">".stripslashes($row['it_basic'])."</div>\n";
     }
-
     if ($this->view_it_price) {
         echo "<div class=\"sct_cost\">\n";
         echo display_price(get_price($row), $row['it_tel_inq'])."\n";
         echo "</div>\n";
     }
+        
+    // 위시리스트 + 공유 버튼 시작 {
+    echo "<div class=\"sct_op_btn\">\n";
+    echo "<button type=\"button\" class=\"btn_wish\" data-it_id=\"{$row['it_id']}\"><span class=\"sound_only\">위시리스트</span><i class=\"fa fa-heart-o\" aria-hidden=\"true\"></i></button>\n";
+    echo "<button type=\"button\" class=\"btn_share\"><span class=\"sound_only\">공유하기</span><i class=\"fa fa-share-alt\" aria-hidden=\"true\"></i></button>\n";
 
-    if ($this->view_it_icon) {
-        echo "<div class=\"sct_icon\">".item_icon($row)."</div>\n";
-    }
-	
-	// 위시리스트 + 공유 버튼 시작 {
-	echo "<div class=\"sct_op_btn\">\n";
-	echo "<button type=\"button\" class=\"btn_wish\" data-it_id=\"{$row['it_id']}\"><span class=\"sound_only\">위시리스트</span><i class=\"fa fa-heart-o\" aria-hidden=\"true\"></i></button>\n";
-	echo "<button type=\"button\" class=\"btn_share\"><span class=\"sound_only\">공유하기</span><i class=\"fa fa-share-alt\" aria-hidden=\"true\"></i></button>\n";
-	
-	echo "<div class=\"sct_sns_wrap\">";
-	if ($this->view_sns) {
+    echo "<div class=\"sct_sns_wrap\">";
+    if ($this->view_sns) {
         $sns_top = $this->img_height + 10;
         $sns_url  = get_pretty_url('shop', $row['it_id']);
         $sns_title = get_text($row['it_name']).' | '.get_text($config['cf_title']);
         echo "<div class=\"sct_sns\">";
-		echo "<h3>SNS 공유</h3>";
+        echo "<h3>SNS 공유</h3>";
         echo get_sns_share_link('facebook', $sns_url, $sns_title, G5_SHOP_SKIN_URL.'/img/facebook.png');
         echo get_sns_share_link('twitter', $sns_url, $sns_title, G5_SHOP_SKIN_URL.'/img/twitter.png');
         echo get_sns_share_link('googleplus', $sns_url, $sns_title, G5_SHOP_SKIN_URL.'/img/gplus.png');
         echo "<button type=\"button\" class=\"sct_sns_cls\"><span class=\"sound_only\">닫기</span><i class=\"fa fa-times\" aria-hidden=\"true\"></i></button>";
         echo "</div>\n";
     }
-	echo "<div class=\"sct_sns_bg\"></div>";
-	echo "</div></div>\n";
-	// } 위시리스트 + 공유 버튼 끝
+    echo "<div class=\"sct_sns_bg\"></div>";
+    echo "</div></div>\n";
+    // } 위시리스트 + 공유 버튼 끝
 
-    echo "</div></li>\n";
+     echo "</div>\n";
+
+    if ($this->view_it_icon) {
+        echo "<div class=\"sct_icon\">".item_icon($row)."</div>\n";
+    }
+    echo "</li>\n";
 }
 
 if ($i > 0) echo "</ul>\n";
