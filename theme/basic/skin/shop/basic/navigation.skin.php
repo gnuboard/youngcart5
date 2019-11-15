ea122dbb91b7c74dfbe3bca6febdaf73bd0cfdbf
thisgun <thisgun@naver.com>
2019-11-15 18:32:40 +0900

    쇼핑몰 카테고리 view 코드 수정

--- theme/basic/skin/shop/basic/navigation.skin.php
+++ theme/basic/skin/shop/basic/navigation.skin.php
@@ -23,42 +23,45 @@ if( !$is_item_view && $ca_id ){
 $location_class = array();
 if($is_item_view){
     $location_class[] = 'view_location';    // view_location는 리스트 말고 상품보기에서만 표시
+} else {
+	$location_class[] = 'is_list is_right';    // view_location는 리스트 말고 상품보기에서만 표시
 }
 
 // add_stylesheet('css 구문', 출력순서); 숫자가 작을 수록 먼저 출력됨
 add_stylesheet('<link rel="stylesheet" href="'.G5_SHOP_CSS_URL.'/style.css">', 0);
+add_javascript('<script src="'.G5_JS_URL.'/shop.category.navigation.js"></script>', 10);
 ?>
 
 <div id="sct_location" class="<?php echo implode(' ', $location_class);?>"> <!-- class="view_location" --> <!-- view_location는 리스트 말고 상품보기에서만 표시 -->
     <a href='<?php echo G5_SHOP_URL; ?>/' class="go_home"><span class="sound_only">메인으로</span><i class="fa fa-home" aria-hidden="true"></i></a>
-    <i class="fa fa-angle-right" aria-hidden="true"></i>
+    <i class="dividing-line fa fa-angle-right" aria-hidden="true"></i>
     <?php if ( is_array($navi_datas) && $navi_datas ){ ?>
 
         <?php if( isset($navi_datas[0]) && $navi_datas[0] ){ ?>
-        <select class="category1">
+        <select class="shop_hover_selectbox category1">
             <?php foreach((array) $navi_datas[0] as $data ){ ?>
                 <option value="<?php echo $data['ca_id']; ?>" data-url="<?php echo $data['url']; ?>" <?php if($ca_ids['ca_id'] === $data['ca_id']) echo 'selected'; ?>><?php echo $data['ca_name']; ?></option>
             <?php } ?>
         </select>
         <?php } ?>
         <?php if( isset($navi_datas[1]) && $navi_datas[1] ){ ?>
-        <i class="fa fa-angle-right" aria-hidden="true"></i>
-        <select class="category2">
+        <i class="dividing-line fa fa-angle-right" aria-hidden="true"></i>
+        <select class="shop_hover_selectbox category2">
             <?php foreach((array) $navi_datas[1] as $data ){ ?>
                 <option value="<?php echo $data['ca_id']; ?>" data-url="<?php echo $data['url']; ?>" <?php if($ca_ids['ca_id2'] === $data['ca_id']) echo 'selected'; ?>><?php echo $data['ca_name']; ?></option>
             <?php } ?>
         </select>
         <?php } ?>
         <?php if( isset($navi_datas[2]) && $navi_datas[2] ){ ?>
-        <i class="fa fa-angle-right" aria-hidden="true"></i>
-        <select class="category3">
+        <i class="dividing-line fa fa-angle-right" aria-hidden="true"></i>
+        <select class="shop_hover_selectbox category3">
             <?php foreach((array) $navi_datas[2] as $data ){ ?>
                 <option value="<?php echo $data['ca_id']; ?>" data-url="<?php echo $data['url']; ?>" <?php if($ca_ids['ca_id3'] === $data['ca_id']) echo 'selected'; ?>><?php echo $data['ca_name']; ?></option>
             <?php } ?>
         </select>
         <?php } ?>
     <?php } else { ?>
-        <?php echo $g5['title']; ?>
+        <?php echo get_text($g5['title']); ?>
     <?php } ?>
 </div>
 <script>
@@ -73,6 +76,8 @@ jQuery(function($){
 
             window.location.href = url;
         });
+
+		$("select.shop_hover_selectbox").shop_select_to_html();
     });
 });
 </script>
